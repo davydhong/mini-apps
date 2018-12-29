bb4f848f8b6061d7ee7c42cb83f447cae657513d
davydhong <davydhong@gmail.com>
2018-12-29 00:02:17 -0800

    Splitted loop (227)

--- refactoring/2.statement/statement.js
+++ refactoring/2.statement/statement.js
@@ -4,15 +4,20 @@ const {
 
 const statement = (invoice) => {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice[0].customer}\n`;
+
   for (const perf of invoice[0].performances) {
-    // add volume credits
-    volumeCredits += volumeCreditsFor(perf);
     // print line for this order
     result += `${playFor(perf).name}:${usd(amountFor(perf) / 100)}(${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
+
+  let volumeCredits = 0;
+  for (const perf of invoice[0].performances) {
+    // add volume credits
+    volumeCredits += volumeCreditsFor(perf);
+  }
+
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
