1dbf4dccfd332c56f4dfe5b68f74422c7c6ac513
davydhong <davydhong@gmail.com>
2018-12-28 23:37:28 -0800

    Inline Variable (123)

--- refactoring/2.statement/statement.js
+++ refactoring/2.statement/statement.js
@@ -8,15 +8,13 @@ const statement = (invoice) => {
   let result = `Statement for ${invoice[0].customer}\n`;
   const format = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format;
   for (const perf of invoice[0].performances) {
-    const thisAmount = amountFor(perf);
-
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
     if (playFor(perf).type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
     // print line for this order
-    result += `${playFor(perf).name}:${format(thisAmount / 100)}(${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += `${playFor(perf).name}:${format(amountFor(perf) / 100)}(${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
