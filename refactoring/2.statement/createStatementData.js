91c096606494bf23284c5e37203afe03602703db
davydhong <davydhong@gmail.com>
2019-01-01 23:29:40 -0800

    rendering html

--- refactoring/2.statement/createStatementData.js
+++ refactoring/2.statement/createStatementData.js
@@ -47,12 +47,7 @@ const volumeCreditsFor = (aPerformance) => {
   return result;
 };
 
-// *
-// *
-// *
-const usd = aNumber => new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(aNumber);
-
-const invoice = require('./invoices');
+// const invoice = require('./invoices');
 
 // *
 // *
@@ -95,17 +90,5 @@ const createStatementData = (invoice) => {
   return statementData;
 };
 
-const renderPlainText = (data) => {
-  let result = `Statement for ${data.customer}\n`;
 
-  for (const perf of data.performances) {
-    // print line for this order
-    result += `${perf.play.name}:${usd(perf.amount / 100)}(${perf.audience} seats)\n`;
-  }
-
-  result += `Amount owed is ${usd(data.totalAmount / 100)}\n`;
-  result += `You earned ${data.totalVolumeCredits} credits\n`;
-  return result;
-};
-
-module.exports = { renderPlainText, createStatementData };
+module.exports = { createStatementData };
--- refactoring/2.statement/statement.js
+++ refactoring/2.statement/statement.js
@@ -1,5 +1,38 @@
-const { renderPlainText, createStatementData } = require('./createStatementData');
+const { createStatementData } = require('./createStatementData');
+
+const usd = aNumber => new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(aNumber / 100);
+
+const renderPlainText = (data) => {
+  let result = `Statement for ${data.customer}\n`;
+
+  for (const perf of data.performances) {
+    // print line for this order
+    result += `${perf.play.name}:${usd(perf.amount)}(${perf.audience} seats)\n`;
+  }
+
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
+  return result;
+};
 
 const statement = invoice => renderPlainText(createStatementData(invoice));
+
+const renderHtml = (data) => {
+  let result = `<h1>Statment for ${data.customer}</h1>\n`;
+  result += '<table>\n';
+  result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>';
+  for (const perf of data.performances) {
+    // print line for this order
+    result += `<tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += '</table>\n';
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+
+  return result;
+};
+
+const htmlStatement = invoice => renderHtml(createStatementData(invoice));
 
 module.exports = statement;
