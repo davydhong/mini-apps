12c7b5a9a98e585f11315ce4114626366a9e251f
davydhong <davydhong@gmail.com>
2019-01-22 14:57:01 -0800

    CombineFunctionsintoClass(144)

--- refactoring/1.bird/helpers.js
+++ refactoring/1.bird/helpers.js
@@ -1,31 +1,38 @@
-function plumage(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 'average';
-    case 'AfricanSwallow':
-      return bird.numberOfCoconuts > 2 ? 'tired' : 'average';
-    case 'NorwegianBlueParrot':
-      return bird.voltage > 100 ? 'scorched' : 'beautiful';
-    default:
-      return 'unknown';
-  }
-}
-function airSpeedVelocity(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 35;
-    case 'AfricanSwallow':
-      return 402 * bird.numberOfCoconuts;
-    case 'NorwegianBlueParrot':
-      return bird.isNailed ? 0 : 10 + bird.voltage / 10;
-    default:
-      return null;
-  }
-}
-
 function plumages(birds) {
   return new Map(birds.map(b => [b.name, plumage(b)]));
 }
 function speeds(birds) {
   return new Map(birds.map(b => [b.name, airSpeedVelocity(b)]));
+}
+
+class Bird {
+  constructor(birdObject) {
+    Object.assign(this, birdObject);
+  }
+
+  get plumage() {
+    switch (this.type) {
+      case 'EuropeanSwallow':
+        return 'average';
+      case 'AfricanSwallow':
+        return this.numberOfCoconuts > 2 ? 'tired' : 'average';
+      case 'NorwegianBlueParrot':
+        return this.voltage > 100 ? 'scorched' : 'beautiful';
+      default:
+        return 'unknown';
+    }
+  }
+
+  get airSpeedVelocity() {
+    switch (this.type) {
+      case 'EuropeanSwallow':
+        return 35;
+      case 'AfricanSwallow':
+        return 402 * this.numberOfCoconuts;
+      case 'NorwegianBlueParrot':
+        return this.isNailed ? 0 : 10 + this.voltage / 10;
+      default:
+        return null;
+    }
+  }
 }
