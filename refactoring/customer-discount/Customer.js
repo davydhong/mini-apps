deafaab45fe685c439e63ae0bcca95ed7aea1f80
davydhong <davydhong@gmail.com>
2019-01-24 00:35:00 -0800

    discount setter with assertion

--- refactoring/customer-discount/Customer.js
+++ refactoring/customer-discount/Customer.js
@@ -1,6 +1,15 @@
 const assert = require('assert');
 
 class Customer {
+  constructor(discountRate) {
+    this._discountRate = discountRate;
+  }
+
+  set discountRate(aNumber) {
+    assert(aNumber === null || aNumber >= 0);
+    this._discountRate = aNumber;
+  }
+
   applyDiscount(aNumber) {
     if (!this.discountRate) return aNumber;
 
