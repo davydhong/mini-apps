31b12d59bcae6dc4613fa93fdd3da6c4d698c375
davydhong <davydhong@gmail.com>
2019-01-25 00:04:11 -0800

    Regular bookings offer a talkback after the show, but only on nonpeak days.

---
+++ refactoring/show-booking/Booking.js
@@ -0,0 +1,22 @@
+class Booking {
+  constructor(show, date) {
+    this._show = show;
+    this._date = date;
+  }
+
+  get hasTalkback() {
+    return this._show.hasOwnProperty('talkback') && !this.isPeakDay;
+  }
+}
+class PremiumBooking extends Booking {
+  constructor(show, date, extras) {
+    super(show, date);
+    this._extras = extras;
+  }
+
+  get hasTalkback() {
+    return {}.hasOwnProperty.call(this._show, 'talkback');
+  }
+}
+
+module.exports = { Booking, PremiumBooking };
