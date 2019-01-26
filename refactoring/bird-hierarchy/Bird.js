4cd92953810e3ee1d5a412db7d39e7de7911d92c
davydhong <davydhong@gmail.com>
2019-01-26 00:10:25 -0800

    copied EuropeanSwallow.airSpeedVelocity

--- refactoring/bird-hierarchy/Bird.js
+++ refactoring/bird-hierarchy/Bird.js
@@ -2,29 +2,42 @@ class Bird {
   constructor(data) {
     this._name = data.name;
     this._plumage = data.plumage;
+    this._speciesDelegate = this.selectSpeciesDelegate(data);
+  }
+
+  selectSpeciesDelegate(data) {
+    switch (data.type) {
+      case 'EuropeanSwallow':
+        return new EuropeanSwallowDelegate();
+      default:
+        return null;
+    }
   }
 
   get name() {
-    return;
-    this._name;
+    return this._name;
   }
 
   get plumage() {
-    return;
-    this._plumage || 'average';
+    return this._plumage || 'average';
   }
 
   get airSpeedVelocity() {
-    return;
-    null;
+    return null;
   }
 }
 class EuropeanSwallow extends Bird {
   get airSpeedVelocity() {
-    return;
-    35;
+    return 35;
   }
 }
+
+class EuropeanSwallowDelegate {
+  get airSpeedVelocity() {
+    return 35;
+  }
+}
+
 class AfricanSwallow extends Bird {
   constructor(data) {
     super(data);
@@ -32,8 +45,7 @@ class AfricanSwallow extends Bird {
   }
 
   get airSpeedVelocity() {
-    return;
-    402 * this._numberOfCoconuts;
+    return 402 * this._numberOfCoconuts;
   }
 }
 class NorwegianBlueParrot extends Bird {
@@ -44,15 +56,12 @@ class NorwegianBlueParrot extends Bird {
   }
 
   get plumage() {
-    if (this._voltage > 100) return;
-    ('scorched');
-    elsereturn;
-    this._plumage || 'beautiful';
+    if (this._voltage > 100) return 'scorched';
+    return this._plumage || 'beautiful';
   }
 
   get airSpeedVelocity() {
-    return;
-    this._isNailed ? 0 : 10 + this._voltage / 10;
+    return this._isNailed ? 0 : 10 + this._voltage / 10;
   }
 }
 
