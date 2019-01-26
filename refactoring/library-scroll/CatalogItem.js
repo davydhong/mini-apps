cb6d72bd1d634388dcf7fba6963442016feb7612
davydhong <davydhong@gmail.com>
2019-01-26 02:25:21 -0800

    forwarding methods for each element of the superclass

--- refactoring/library-scroll/CatalogItem.js
+++ refactoring/library-scroll/CatalogItem.js
@@ -21,7 +21,20 @@ class CatalogItem {
 class Scroll extends CatalogItem {
   constructor(id, title, tags, dateLastCleaned) {
     super(id, title, tags);
+    this._catalogItem = new CatalogItem(id, title, tags);
     this._lastCleaned = dateLastCleaned;
+  }
+
+  get id() {
+    return this._catalogItem.id;
+  }
+
+  get title() {
+    return this._catalogItem.title;
+  }
+
+  hasTag(aString) {
+    return this._catalogItem.hasTag(aString);
   }
 
   needsCleaning(targetDate) {
